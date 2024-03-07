4d3c86a42417c3d93777a28204eabfcdf0bad466
Gabriel Cabrejas <gabriel@cabrejas.net>
2024-03-07 21:46:38 +0100

    veamos

--- api/grades_report/router.js
+++ api/grades_report/router.js
@@ -1,13 +1,25 @@
 const router = require('express').Router()
+const redis = require('redis')
+const { promisify } = require('util')
 
-const data = [] // in-memory db
+// Create a Redis client
+const client = redis.createClient(process.env.REDIS_URL)
+const getAsync = promisify(client.get).bind(client)
+const setAsync = promisify(client.set).bind(client)
 
-router.post('/report', (req, res, next) => {
-  data.push(req.body)
-  res.json(req.body)
+router.post('/report', async (req, res) => {
+  const newData = req.body
+  // Retrieve existing data
+  const data = JSON.parse(await getAsync('data')) || []
+  data.push(newData)
+  // Update data in Redis
+  await setAsync('data', JSON.stringify(data))
+  res.json(newData)
 })
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  // Retrieve and send data
+  const data = JSON.parse(await getAsync('data')) || []
   res.json(data)
 })
 
